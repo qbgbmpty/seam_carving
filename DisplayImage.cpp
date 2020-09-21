6ead8ca7e584927133f1620d7d43a11a1d5211b5
qbgbmpty <qbgbmpty@gmail.com>
2020-09-21 16:16:36 +0800

    Gray Channel with output energy map

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -7,8 +7,7 @@ int main(int argc, char* argv[]) {
 
 	Mat img = imread(argv[1]);
 	Mat img_backup = img.clone();
-	//Mat energyMat(img.rows,img.cols,CV_8U,cv::Scalar(0));
-	int energyMat[img.rows][img.cols];
+	Mat energyMat(img.rows,img.cols,CV_8U,cv::Scalar(0));
 	
 	if (3 == img.channels())
 		cvtColor(img, img, cv::COLOR_BGR2GRAY);
@@ -21,17 +20,17 @@ int main(int argc, char* argv[]) {
 			{
 				if (j==0)
 				{
-					energyMat[i][j]=(abs((*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2)+(*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * i + img.step[1] * (j+1))*2)));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2)+(*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * i + img.step[1] * (j+1))*2)))/9;
 				}
 
 				else if (j==img.cols-1)
 				{
-					energyMat[i][j]=(abs((*(img.data + img.step[0] * (i+1 )+ img.step[1] * j)*2)+(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))))+abs((*(img.data + img.step[0] * i + img.step[1] * (j-1))*2)));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((*(img.data + img.step[0] * (i+1 )+ img.step[1] * j)*2)+(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))))+abs((*(img.data + img.step[0] * i + img.step[1] * (j-1))*2)))/9;
 				}
 
 				else
 				{
-					energyMat[i][j]=(abs((0+0+0)-(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((0+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1)))-(0+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1)))));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((0+0+0)-(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((0+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1)))-(0+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1)))))/9;
 				}
 			}
 
@@ -39,151 +38,132 @@ int main(int argc, char* argv[]) {
 			{
 				if (j==0)
 				{
-					energyMat[i][j]=(abs((0+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1 )+ img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+0)));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((0+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1 )+ img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+0)))/9;
 				}
 
 				else if (j==img.cols-1)
 				{
-					energyMat[i][j]=(abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+0))+abs(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+0));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+0))+abs(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+0))/9;
 				}
 
 				else
 				{
-					energyMat[i][j]=(abs(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1)))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+0)-(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+0)));
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1)))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+0)-(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+0)))/9;
 				}
 			}
 
 			else
 			{
-				energyMat[i][j]=(abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1)))-(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+*(img.data + img.step[0] *( i+1) + img.step[1] * (j+1)))-(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1)))));
+				*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=(abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i-1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1)))-(*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1))+*(img.data + img.step[0] * (i+1) + img.step[1] * j)*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j+1))))+abs((*(img.data + img.step[0] * (i-1) + img.step[1] * (j+1))+*(img.data + img.step[0] * i + img.step[1] * (j+1))*2+*(img.data + img.step[0] *( i+1) + img.step[1] * (j+1)))-(*(img.data + img.step[0] * (i-1) + img.step[1] * (j-1))+*(img.data + img.step[0] * i + img.step[1] * (j-1))*2+*(img.data + img.step[0] * (i+1) + img.step[1] * (j-1)))))/9;
 			}      
 		}
 		
-	//imshow("img_energy", energyMat);
+	imshow("img_energy", energyMat);
 	
-	//imwrite("out.jpg",energyMat);	
+	imwrite("out.jpg",energyMat);	
 
   
   
-	for (int i = 1; i < img.rows; ++i)
-		for (int j = 0; j < img.cols; ++j)
+	for (int i = 1; i < energyMat.rows; ++i)
+		for (int j = 0; j < energyMat.cols; ++j)
 		{
 			if (j==0)
 			{
-				if (energyMat[i-1][j]<=energyMat[i-1][j+1])
-				{   
-					energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j];
+				if (*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j)<=*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j+1)))
+				{          
+				  *(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j);
 				}
 				else
-				{        
-					energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j+1];          
+				{          
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j+1));          
 				}
 				
 			}
 			
 			else if (j==img.cols-1)
 			{
-				if (energyMat[i-1][j]<=energyMat[i-1][j-1])
+				if (*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j)<=*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j-1)))
 				{
-					energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j];
+				  *(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j);
 				}
 				else
 				{
-					energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j-1];
+				  *(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j-1));
 				}
 			}
 			
 			else
 			{
-				if (energyMat[i-1][j]<=energyMat[i-1][j-1])
+				if (*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j)<=*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j-1)))
 				{
-					if (energyMat[i-1][j]<=energyMat[i-1][j+1])
+					if (*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j)<=*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j+1)))
 					{
-						energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j];
+						*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j);
 					}
 					else
 					{
-						energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j+1];
+					*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j+1));
 					}
 				}
 				else
 				{
-					if (energyMat[i-1][j-1]<=energyMat[i-1][j+1])
+					if (*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j-1))<=*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j+1)))
 					{
-						energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j-1];
+						*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * (j-1));
 					}
 					else
 					{
-						energyMat[i][j]=energyMat[i][j]+energyMat[i-1][j+1];
+						*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * j)+*(energyMat.data + energyMat.step[0] * (i-1) + energyMat.step[1] * j+1);
 					}
 				}
 			}
 		}
-/*
-	for (int i = 0; i < img.rows; ++i)
-	{
-		for (int j = 0; j < img.cols; ++j)
-			printf("%d,",energyMat[i][j]);
-		printf("\n");
-	}
-		*/
+
+  
 	
+  /*
 	int min=9999,min_pos=0;
 	for (int j = 0; j < img.cols; ++j)
-		if (min>energyMat[img.rows-1][j])
-		{
-		  min=energyMat[img.rows-1][j];
-		  min_pos=j;
-		}
-
-	*(img.data + img.step[0] * (img.rows-1) + img.step[1] * min_pos + img.elemSize1() * 0) = 0;
-	*(img.data + img.step[0] * (img.rows-1) + img.step[1] * min_pos + img.elemSize1() * 1) = 0;
-	*(img.data + img.step[0] * (img.rows-1) + img.step[1] * min_pos + img.elemSize1() * 2) = 255;
-	
-  int wtf=img.cols;
-	for (int i = img.rows-2; i >=0 ; --i)
-	{
-		if (min_pos==0)
-		{
-      if (energyMat[i][min_pos]>=energyMat[i][min_pos+1])
-				min_pos=min_pos+1;
-    }	
-
-		else if (min_pos==wtf-1)
+		if (min>*(energyMat.data + energyMat.step[0] * (img.rows-1) + energyMat.step[1] * j))
     {
-      if (energyMat[i][min_pos]>=energyMat[i][min_pos-1])
-				min_pos=min_pos-1;
+      min=*(energyMat.data + energyMat.step[0] * (img.rows-1) + energyMat.step[1] * j);
+      min_pos=j;
     }
 
-		else 
+	*(img.data + img.step[0] * (energyMat.rows-1) + img.step[1] * min_pos + img.elemSize1() * 0) = 0;
+	*(img.data + img.step[0] * (energyMat.rows-1) + img.step[1] * min_pos + img.elemSize1() * 1) = 0;
+	*(img.data + img.step[0] * (energyMat.rows-1) + img.step[1] * min_pos + img.elemSize1() * 2) = 255;
+	
+	for (int i = energyMat.rows-2; i >=0 ; --i)
+	{
+		if (min_pos==0)
+			if (*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * min_pos)>=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos+1)))
+				min_pos=min_pos+1;
+			
+		
+		else if (min_pos==img.cols-1)
+			if (*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * min_pos)>=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos-1)))
+				min_pos=min_pos-1;
+		
+		
+		else
 		{
-			if (energyMat[i][min_pos]>energyMat[i][min_pos-1])
+			if (*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * min_pos)>=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos-1)))
 			{
-				if (energyMat[i][min_pos-1]>energyMat[i][min_pos+1])
-        {
-          printf("%d,%d\n",energyMat[i][min_pos-1],energyMat[i][min_pos+1]);
+				if (*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * min_pos)>=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos+1)))
 					min_pos=min_pos+1;
-        }
-
 				else
-        {
-          printf("%d,%d\n",energyMat[i][min_pos-1],energyMat[i][min_pos+1]);
-          min_pos=min_pos-1;
-        }
-					
+					min_pos=min_pos-1;
 			}
 			else
 			{
-				if (energyMat[i][min_pos]>energyMat[i][min_pos+1])
-        {
-          printf("%d,%d\n",energyMat[i][min_pos],energyMat[i][min_pos+1]);
-          min_pos=min_pos+1;
-        }
-					
-				
+				if (*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos-1))<=*(energyMat.data + energyMat.step[0] * i + energyMat.step[1] * (min_pos+1)))
+					min_pos=min_pos-1;
+				else
+					min_pos=min_pos+1;
 			}
 		}
-		//printf("%d,%d\n",min_pos,img.cols);
+		
 		
 		
 		
@@ -194,7 +174,7 @@ int main(int argc, char* argv[]) {
 	}
 	
 	imshow("img_rst", img_backup);
-	imwrite("img_rst.jpg", img_backup);
+	*/
     
  
 	
